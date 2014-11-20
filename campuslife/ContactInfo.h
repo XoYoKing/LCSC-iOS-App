b677c26f19c71c3cbcb5229c756556e85cf9a675
cmyager <cmyager@lcmail.lcsc.edu>
2014-11-20 11:00:28 -0800

    added new tabs and junk

---
+++ campuslife/ContactInfo.h
@@ -0,0 +1,13 @@
+//
+//  ContactInfo.h
+//  LCSC
+//
+//  Created by Clayton Yager on 11/20/14.
+//  Copyright (c) 2014 LCSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactInfo : UITableViewController
+
+@end
---
+++ campuslife/ContactInfo.m
@@ -0,0 +1,110 @@
+//
+//  ContactInfo.m
+//  LCSC
+//
+//  Created by Clayton Yager on 11/20/14.
+//  Copyright (c) 2014 LCSC. All rights reserved.
+//
+
+#import "ContactInfo.h"
+
+@interface ContactInfo ()
+
+@end
+
+@implementation ContactInfo
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    // Return the number of rows in the section.
+    return 3;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellCount" forIndexPath:indexPath];
+    UILabel *titleLabel = (UILabel *)[cell viewWithTag:1];
+    UILabel *numberLabel = (UILabel *)[cell viewWithTag:2];
+    if(indexPath.row == 0){
+        titleLabel.text = @"For Emergencies Only";
+        numberLabel.text = @"911";
+    }else if (indexPath.row == 1){
+        titleLabel.text = @"Campus Security (Non-Emergencies)";
+        numberLabel.text = @"208-792-2815";
+    }else if (indexPath.row == 2){
+        titleLabel.text = @"Campus Emergency Information Line";
+        numberLabel.text = @"208-792-5272";
+    }
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
