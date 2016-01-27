2d738fe3a9ee5147f7997fd3a1b71d55c641cbc6
kwshoemaker2 <kyleshoe2@gmail.com>
2016-01-27 11:11:56 -0800

    Created the CalendarInfo class, which contains metadata about the calendars that needs to be used in multiple places in the app. This is part of modularizing the MonthlyEvents class.

---
+++ campuslife/CalendarInfo.h
@@ -0,0 +1,13 @@
+//
+//  CalendarInfo.h
+//  LCSC
+//
+//  Created by Student on 1/27/16.
+//  Copyright (c) 2016 LCSC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalendarInfo : NSObject
+
+@end
---
+++ campuslife/CalendarInfo.m
@@ -0,0 +1,13 @@
+//
+//  CalendarInfo.m
+//  LCSC
+//
+//  Created by Student on 1/27/16.
+//  Copyright (c) 2016 LCSC. All rights reserved.
+//
+
+#import "CalendarInfo.h"
+
+@implementation CalendarInfo
+
+@end
