a88270cf7d56e74c2315d7d31505573f5ad42c5e
kwshoemaker2 <kyleshoe2@gmail.com>
2016-03-09 18:11:03 -0800

    Removed and then re-added the bridging header

---
+++ LCSC-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  LCSC-Bridging-Header.h
+//  LCSC
+//
+//  Created by Computer Science on 3/9/16.
+//  Copyright © 2016 LCSC. All rights reserved.
+//
+
+#ifndef LCSC_Bridging_Header_h
+#define LCSC_Bridging_Header_h
+#import "SWRevealViewController.h"
+
+#endif /* LCSC_Bridging_Header_h */
