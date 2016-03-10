afe05fae1837e7c1e664b95bdd8055e2ecf0e4f4
kwshoemaker2 <kyleshoe2@gmail.com>
2016-03-09 18:11:44 -0800

    Added the views from Eric's storyboard and added the supporting files as well

---
+++ campuslife/BackTableVC.swift
@@ -0,0 +1,33 @@
+//
+//  BackTableVC.swift
+//  SlideoutMenu
+//
+//  Created by Eric de Baere Grassl on 3/3/16.
+//  Copyright © 2016 Eric de Baere Grassl. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BackTableVC: UITableViewController {
+    
+    var TableArray = [String]()
+    
+    override func viewDidLoad() {
+        TableArray = ["Main Page", "LCSC Website","Athetics Website","Warrior Web", "LCMail", "Profile"]
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TableArray.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(TableArray[indexPath.row], forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text =  TableArray[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
