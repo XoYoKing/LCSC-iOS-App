c88e8bf17b9340daafb2c4ac9e33f74bd3bc9db9
Bryan Tomey <batomey@bryans-macbook-pro.local>
2016-04-20 12:40:25 -0700

    PanGesture added to Campus Map

--- campuslife/MapViewController.swift
+++ campuslife/MapViewController.swift
@@ -12,15 +12,28 @@ import UIKit
 class MapViewController: UIViewController {
     @IBOutlet weak var menuButton: UIBarButtonItem!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var pinchView: UIView!
+    
+    let pinchRec = UIPinchGestureRecognizer()
+    func pinchedView(sender:UIPinchGestureRecognizer){
+        self.view.bringSubviewToFront(pinchView)
+        sender.view!.transform = CGAffineTransformScale(sender.view!.transform, sender.scale, sender.scale)
+        sender.scale = 1.0
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //my code :)
         //loads the slide menu function
         menuButton.target = revealViewController()
-        menuButton.action = Selector("revealToggle:")
+        menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        pinchRec.addTarget(self, action: #selector(MapViewController.pinchedView(_:)))
+        pinchView.addGestureRecognizer(pinchRec)
+        pinchView.userInteractionEnabled = true
+        pinchView.multipleTouchEnabled = true
         //imageView.image = UIImage(contentsOfFile: "CampusMap")
     }
 }
