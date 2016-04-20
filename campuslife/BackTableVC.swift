d216233afea16b01adc027a6a47321f17b7ea217
kwshoemaker2 <kyleshoe2@gmail.com>
2016-04-20 11:27:12 -0700

    Added icons and moved stuff around on the menu

--- campuslife/BackTableVC.swift
+++ campuslife/BackTableVC.swift
@@ -23,18 +23,18 @@ class BackTableVC: UITableViewController {
         //fill the menu with the itens listed on the arrays
         TableArray = ["Main Page","Calendar","Emergency", "Campus Map", "Radio", "Athletics Videos", "Hangouts", "LCSC Website", "Athletics Website", "LCMail", "Warrior Web", "BlackBoard", "Profile"]
     }
-    
+    /*
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return TableArray.count
-    }
-    
+    }*/
+    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(TableArray[indexPath.row], forIndexPath: indexPath) as UITableViewCell
         
         cell.textLabel?.text =  TableArray[indexPath.row]
         
         return cell
-    }
+    }*/
     //Social network links
 //    @IBAction func gotoInstagram(sender: AnyObject) {
 //        if let url = NSURL(string: "http://www.instagram.com/lewisclarkstate/") {
